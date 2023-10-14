aa5e05167b53f6f2914336e9e51549d37a6a63d8
Replit user <>
2023-10-14 19:28:15 +0000

    new try

--- heap.c
+++ heap.c
@@ -72,7 +72,7 @@ void heap_pop(Heap *pq) {
     pq->heapArray[index] = pq->heapArray[2*index + 1];
     pq->heapArray[2*index + 1] = temp;
 
-    index = [2*index + 1];
+    index = pq->heapArray[2*index + 1];
   }
 
 
